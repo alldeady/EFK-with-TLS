478e3554cc720783dff2480f489b02a122f0b247
alldeady <alldeady@gmail.com>
2021-03-08 18:27:51 +0300

    fix certs

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-VERSION="7.8.0" HOSTCERTSDIR="$PWD/certs" ESCERTSDIR="/usr/share/elasticsearch/config" KIBCERTSDIR="/usr/share/kibana/config" FLUCERTSDIR="/etc/ssl/certs" docker-compose up
+VERSION="7.11.1" HOSTCERTSDIR="$PWD/certs" ESCERTSDIR="/usr/share/elasticsearch/config" KIBCERTSDIR="/usr/share/kibana/config" FLUCERTSDIR="/etc/ssl/certs" docker-compose up
