be031ea83cdc1d0727dbe037e654246303c6fd83
alldeady <alldeady@gmail.com>
2021-03-09 14:53:06 +0300

    some fixes

--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
-#!/bin/bash
+# !/bin/bash
 
-VERSION="7.11.1" HOSTCERTSDIR="$PWD/certs" ESCERTSDIR="/usr/share/elasticsearch/config" KIBCERTSDIR="/usr/share/kibana/config" FLUCERTSDIR="/etc/ssl/certs" docker-compose up
+if [[ "$OSTYPE" == "linux"* ]]; then
+        # Change VM linux
+        sudo sysctl -w vm.max_map_count=262144
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Change VM MacOS
+        docker-machine ssh
+        sudo sysctl -w vm.max_map_count=262144
+else
+        exit 1
+fi
+
+VERSION="7.11.1" HOSTCERTSDIR="$PWD/certs" ESCERTSDIR="/usr/share/elasticsearch/config" \
+KIBCERTSDIR="/usr/share/kibana/config" FLUCERTSDIR="/etc/ssl/certs" docker-compose up
